0ef9998296db4d12c583d94f21390e712f451d01
arjunuvladimer <arjunuvladimer@gmail.com>
2022-06-07 11:56:19 +0530

    api

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const app = express()
+const userRoute = require('./app/api/routes/users')
+const { default: mongoose } = require('mongoose')
+
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use('/user',userRoute)
+
+
+app.get('/', (req,res) => {
+    res.json({
+        "APP": "JWT Based API Application",
+        "message": "Successfully Running the Application"
+    })
+})
+
+const mongoURI = "mongodb+srv://arjunuvlad:arjun123@hitman24.ct1jy.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(mongoURI)
+.then(() => {
+    console.log("Successfully Connected to the Database")
+})
+.catch((err) => {
+    console.log(err)
+})
+
+app.listen(5000,() => {
+    console.log("Successfully Running on the PORT: 5000")
+})
